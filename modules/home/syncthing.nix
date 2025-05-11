be6a6d702f5b19ef8daa936e61a3f096d673f36e
backwardspy <backwardspy@pigeon.life>
2025-05-11 20:19:17 +0100

    feat(syncthing): improve device handling

--- modules/home/syncthing.nix
+++ modules/home/syncthing.nix
@@ -1,46 +1,42 @@
 { lib, hostname, ... }:
 let
   # devices with nix-managed syncthing need to add other nix devices but not themselves
-  nixDevice = { name, id }: lib.optionalAttrs (hostname != name) { "${name}" = { inherit id; }; };
+  devices = [
+    {
+      name = "hyperion";
+      id = "5RVRAO2-TK4ITIR-W6O3W4Y-PHW5FPZ-PGOA7LR-M36WLMJ-JHWYMNU-JQYI2QI";
+    }
+    {
+      name = "galactica";
+      id = "WO27XPZ-TZ5O5MX-A3FQWOO-FAZG6OV-R7ZHLAM-3J3APEZ-EWAND3O-U267MAW";
+    }
+    {
+      name = "zephyr";
+      id = "LHY5B44-CMYADIP-FKENFCK-TCBLRGX-5AXV6D3-4AMYVIG-GYUFRAQ-K7NRTQV";
+    }
+    {
+      name = "kujira";
+      id = "RULL2TJ-MJKMACY-ZKW5MPC-3X345JR-3YZ6OVI-SP62GWQ-J3V3F42-BQYRXAZ";
+    }
+  ];
+  otherDevices = (lib.filter (device: device.name != hostname) devices);
+  otherDeviceNames = map (device: device.name) otherDevices;
 in
 {
   services.syncthing = {
     enable = true;
     settings = {
-      devices =
-        {
-          "hyperion".id = "5RVRAO2-TK4ITIR-W6O3W4Y-PHW5FPZ-PGOA7LR-M36WLMJ-JHWYMNU-JQYI2QI";
-          "galactica".id = "WO27XPZ-TZ5O5MX-A3FQWOO-FAZG6OV-R7ZHLAM-3J3APEZ-EWAND3O-U267MAW";
-        }
-        // (nixDevice {
-          name = "zephyr";
-          id = "LHY5B44-CMYADIP-FKENFCK-TCBLRGX-5AXV6D3-4AMYVIG-GYUFRAQ-K7NRTQV";
-        })
-        // (nixDevice {
-          name = "kujira";
-          id = "RULL2TJ-MJKMACY-ZKW5MPC-3X345JR-3YZ6OVI-SP62GWQ-J3V3F42-BQYRXAZ";
-        });
+      devices = lib.mapAttrs (name: value: { id = value; }) (
+        lib.listToAttrs (map ({ name, id }: lib.nameValuePair name id) otherDevices)
+      );
       folders = {
-        "~/camera" = {
-          id = "pixel_7_pro_3prk-photos";
-          devices = [
-            "hyperion"
-            "galactica"
-          ];
-        };
         "~/notes" = {
           id = "notes";
-          devices = [
-            "hyperion"
-            "galactica"
-          ];
+          devices = otherDeviceNames;
         };
         "~/sync" = {
           id = "sync";
-          devices = [
-            "hyperion"
-            "galactica"
-          ];
+          devices = otherDeviceNames;
         };
       };
     };
